812fa1b4022783060059257b6647d64d9676e7a4
Gustavo Clay <gustavoclay@gmail.com>
2018-12-28 19:05:34 -0200

    Curso Angular parte 1: Fundamentos 06 Angular CLI, instalação e criação do projeto

--- alura/alurapic/src/app/app.component.ts
+++ alura/alurapic/src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'alurapic';
+
+  url = 'https://www.gettyimages.com/gi-resources/images/CreativeLandingPage/HP_Sept_24_2018/CR3_GettyImages-159018836.jpg';
+  description = 'Flamingos';
 }
