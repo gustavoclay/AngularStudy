2ce69d940be30d975e82bc0f6b34c830841dc7fd
Gustavo Clay <gustavoclay@gmail.com>
2019-01-02 16:00:25 -0200

    Curso Angular parte 1: Fundamentos Aula 04 Atividade 8

--- alura/alurapic/src/app/photos/photos.module.ts
+++ alura/alurapic/src/app/photos/photos.module.ts
@@ -3,11 +3,13 @@ import { PhotoComponent } from './photo/photo.component';
 import { HttpClientModule } from '@angular/common/http';
 import { PhotoListComponent } from './photo-list/photo-list.component';
 import { CommonModule } from '@angular/common';
+import { PhotoFormComponent } from './photo-form/photo-form.component';
 
 @NgModule({
   declarations: [
     PhotoComponent,
-    PhotoListComponent
+    PhotoListComponent,
+    PhotoFormComponent
   ],
   imports: [
     HttpClientModule,
