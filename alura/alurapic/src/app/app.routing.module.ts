2163f5f12e5c4f619f5c6e3beebe2c08a529241c
Gustavo Clay <gustavoclay@gmail.com>
2019-01-02 16:16:04 -0200

    Curso Angular parte 1: Fundamentos Aula 04 Atividade 10

--- alura/alurapic/src/app/app.routing.module.ts
+++ alura/alurapic/src/app/app.routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { PhotoListComponent } from './photos/photo-list/photo-list.component';
 import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
 
 const routes: Routes = [
   {path: 'user/flavio', component: PhotoListComponent},
-  {path: 'p/add', component: PhotoFormComponent}
+  {path: 'p/add', component: PhotoFormComponent},
+  {path: '**', component: NotFoundComponent}
 ];
 
 @NgModule({
