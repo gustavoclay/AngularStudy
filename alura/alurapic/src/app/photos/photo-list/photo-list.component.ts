3474b77e3a5c06fe3656f9b5d73e0d14556ae2a2
Gustavo Clay <gustavoclay@gmail.com>
2019-01-02 19:09:12 -0200

    Curso Angular parte 1: Fundamentos Aula 06 Atividade 09

--- alura/alurapic/src/app/photos/photo-list/photo-list.component.ts
+++ alura/alurapic/src/app/photos/photo-list/photo-list.component.ts
@@ -1,22 +1,30 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {Photo} from '../photo';
+import {Subject} from 'rxjs';
+import {debounceTime} from 'rxjs/operators';
 
 @Component({
     selector: 'app-photo-list',
     templateUrl: './photo-list.component.html',
     styleUrls: ['./photo-list.component.css']
 })
-export class PhotoListComponent implements OnInit {
+export class PhotoListComponent implements OnInit, OnDestroy {
 
     photos: Photo[] = [];
     filter = '';
+    debounce = new Subject<string>();
 
     constructor(private activatedRoute: ActivatedRoute) {
     }
 
     ngOnInit(): void {
         this.photos = this.activatedRoute.snapshot.data['photos'];
+        this.debounce.pipe(debounceTime(300)).subscribe(filter => this.filter = filter);
+    }
+
+    ngOnDestroy(): void {
+        this.debounce.unsubscribe();
     }
 
 }
