b1ea3b548c0f17fc01ceaf2a4df6df603dfef4d6
Gustavo Clay <gustavoclay@gmail.com>
2019-01-03 15:13:06 -0200

    Curso Angular parte 1: Fundamentos Aula 07 Atividade 04

---
+++ alura/alurapic/src/app/home/home.module.ts
@@ -0,0 +1,11 @@
+import {NgModule} from '@angular/core';
+import {SignInComponent} from './signin/signin.component';
+
+@NgModule({
+    imports: [],
+    exports: [],
+    declarations: [SignInComponent],
+    providers: [],
+})
+export class HomeModule {
+}
