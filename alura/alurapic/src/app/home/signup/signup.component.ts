27aaadc2517ecd53b0b6de07cbdb973b6b18a87a
Gustavo Clay <gustavoclay@gmail.com>
2019-01-04 18:22:15 -0200

    Curso Angular parte 2: Autenticação, Forms e lazy loading Aula 02 Atividade 6

--- alura/alurapic/src/app/home/signup/signup.component.ts
+++ alura/alurapic/src/app/home/signup/signup.component.ts
@@ -1,8 +1,51 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {lowerCaseValidator} from '../../shared/validators/lower-case.validator';
 
 @Component({
     templateUrl: 'signup.template.html'
 })
-export class SignupComponent {
-    
+export class SignupComponent implements OnInit {
+
+    signupForm: FormGroup;
+
+    constructor(private formBuilder: FormBuilder) {
+
+    }
+
+    ngOnInit(): void {
+        this.signupForm = this.formBuilder.group({
+            email: ['',
+                [
+                    Validators.required,
+                    Validators.email
+                ]
+            ],
+            fullName: ['',
+                [
+                    Validators.required,
+                    Validators.minLength(1),
+                    Validators.maxLength(40)
+                ]
+            ],
+            userName: ['',
+                [
+                    Validators.required,
+                    lowerCaseValidator,
+                    Validators.minLength(2),
+                    Validators.maxLength(30)
+                ]
+            ],
+            password: ['',
+                [
+                    Validators.required,
+                    Validators.minLength(8),
+                    Validators.maxLength(14)
+                ]
+            ]
+        })
+        ;
+    }
+
+
 }
